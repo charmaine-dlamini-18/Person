0aa72822ed139f85905864d3a29725d50da66d69
sabelo-ceza-26 <220094489@mycput.ac.za>
2026-02-14 13:45:23 +0200

    first commit

---
+++ src/main/java/za/ac/cput/domain/Person.java
@@ -0,0 +1,50 @@
+package za.ac.cput.domain;
+
+public class Person {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
